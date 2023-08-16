b61e7dc2f2fbbd9d34d18cd7927a14e7cc588e47
steve biko <biko.nyamai@gmail.com>
2023-08-16 08:29:46 +0300

    shutdown the PC after training models

--- image_classification_advance/good_night.py
+++ image_classification_advance/good_night.py
@@ -3,7 +3,17 @@ from inception_net_v1 import main as inception
 from minception import main as minception
 from pretrained_inception_resnet_v2 import main as inception_resnet_v2
 from utils import init_gpus
+import os
+def shutdown():
+    print("Shutting down")
+    # Execute the shutdown command based on the operating system
+    if os.name == "posix":  # Unix/Linux/Mac
 
+        os.system("sudo shutdown -h now")
+    elif os.name == "nt":  # Windows
+        os.system("shutdown /s /f /t 0")
+    else:
+        print("Unsupported operating system")
 if __name__ == '__main__':
     init_gpus()
     inception_resnet_v2()
